7bdda9153af3fab17c494b5d254f0abc5ef60488
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2024-11-06 12:57:14 -0500

    Update packages, point plot-o-meter to mainnet

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { spacePledge } from '@autonomys/auto-consensus';
+import { spacePledge, spacePledged } from '@autonomys/auto-consensus';
 import { activate } from '@autonomys/auto-utils';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -11,7 +11,7 @@ let api;
 
 // Activate the API on server start
 (async () => {
-  api = await activate();
+  api = await activate({ networkId: 'mainnet' });
 })();
 
 // Serve static files from the 'public' directory
@@ -23,8 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // API endpoint to get spacePledged data
 app.get('/api/space-pledge', async (req, res) => {
   try {
-    const spacePledged = await spacePledge(api);
-    res.json({ spacePledged: spacePledged.toString() }); // Send as string to handle BigInt
+    const spacePledge = await spacePledged(api);
+    console.log(spacePledge);
+    res.json({ spacePledged: spacePledge.toString() }); // Send as string to handle BigInt
   } catch (error) {
     console.error('Error fetching space pledged:', error);
     res.status(500).json({ error: 'Failed to fetch space pledge' });
